6e09038769d31a2f9bfd6d41da4d337e753c77b2
coderzaman <coder.aktar@gmail.com>
2025-02-19 00:26:16 +0600

    Product details

--- ShopApp/urls.py
+++ ShopApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'shop_app'
 
 urlpatterns = [
-    path('', view=views.home.as_view(), name='home' )
+    path('', view=views.home.as_view(), name='home' ),
+    path('product/<pk>/', view=views.ProductDetail.as_view(), name='product' ),
 ]
--- ShopApp/views.py
+++ ShopApp/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
 # Import Views
-from django.views.generic import ListView, DeleteView 
+from django.views.generic import ListView, DetailView 
 
 # Import Models
 from .models import Product
 
-# Create your views here.
+#Mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+
+# Create your views here.
 class home(ListView):
     model = Product
     template_name = 'ShopApp/home.html'
+    
+
+class ProductDetail(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'ShopApp/product.html'
